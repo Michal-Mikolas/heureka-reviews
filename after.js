d423be7da1726e764819e2d52343d6da1933d718
Michal Mikolas <nanuqcz@gmail.com>
2022-04-07 14:36:12 +0200

    Added first working version of the extension.

---
+++ after.js
@@ -0,0 +1,15 @@
+// this code will be executed after page load
+(function() {
+  $ = jQuery;
+
+  $('.c-product-list__item').each(function(){
+    $product = $(this);
+    $a = $product.find('a.c-product__image-link')
+
+    $product.find('h3').append('<div class="rating"></div>');
+    $product.find('h3 .rating').load(
+      $a.attr('href') + ' .c-top-position__rating'
+    )
+  })
+
+})();
