74001be1ed80bbc9e2ceb1b7ac5f292f1c51988c
Michal Mikolas <nanuqcz@gmail.com>
2022-04-08 00:32:16 +0200

    Added extra sorting feature.

--- src/after.js
+++ src/after.js
@@ -1,16 +1,28 @@
 // this code will be executed after page load
 (function() {
+
   //
   // Init
   //
   $ = jQuery;
 
+
   //
   // Functions
   //
-  refreshRating = function(){
+  var refreshProductData = function($product) {
+    var score = $product.find('.c-top-position__rating .c-rating-widget__value').text().replace(/[^\d]/g, '') * 1;
+    var amount = $product.find('.c-top-position__rating .c-pipe-list__item.e-link').text().replace(/[^\d]/g, '') * 1;
+    var shops = $product.find('.c-product__shops').text().replace(/[^\d]/g, '') * 1;
+
+    $product.attr('data-reviews-score', score);
+    $product.attr('data-reviews-amount', amount);
+    $product.attr('data-shops', shops);
+  };
+
+  var refreshRating = function(){
     $('.c-product-list__item.o-grid__item').each(function(){
-      $product = $(this);
+      var $product = $(this);
 
       if ($product.find('h3 .rating').length) {
         return;
@@ -20,16 +32,19 @@
 
       $product.find('h3').append('<div class="rating"></div>');
       $product.find('h3 .rating').load(
-        $a.attr('href') + ' .c-top-position__rating'
+        $a.attr('href') + ' .c-top-position__rating', 
+        function(){ refreshProductData($product); }
       )
+
+      refreshProductData($product);  // support for list view
     })
   };
 
-  isListLoaded = function(){
+  var isListLoaded = function(){
     return ($('.is-fetching').length == 0) || ($('.c-product-list__item.o-grid__item').length == 0);
   }
 
-  refreshRatingManager = function(){
+  var refreshRatingManager = function(){
     if (!isListLoaded()) {
       setTimeout(refreshRatingManager, 1000);
 
@@ -42,9 +57,47 @@
     }
   };
 
+  var sortProducts = function($link){
+    $('.c-subtabs__item').removeClass('is-active');
+    $link.addClass('is-active');
+
+    var $products = $('.c-product-list__item');
+    $products.sort(function(a, b){
+      return $(b).data($link.data('sort-by')) - $(a).data($link.data('sort-by'));
+    });
+    $(".c-product-list__items").html($products);
+  };
+
+
   //
   // Do the job
   //
+
+  // Add stars summary
   refreshRatingManager();
+  
+  // Add new sort features
+  $('.c-subtabs__list').append('\
+    <li class="c-subtabs__item">\
+      <a rel="nofollow" class="c-subtabs__link sort-by" data-sort-by="reviews-score" href="#">\
+        <span>Nejvíce hvězdiček</span>\
+      </a>\
+    </li>\
+    <li class="c-subtabs__item">\
+      <a rel="nofollow" class="c-subtabs__link sort-by" data-sort-by="reviews-amount" href="#">\
+        <span>Největší počet hodnocení</span>\
+      </a>\
+    </li>\
+    <li class="c-subtabs__item">\
+      <a rel="nofollow" class="c-subtabs__link sort-by" data-sort-by="shops" href="#">\
+        <span>Nejvíce obchodů</span>\
+      </a>\
+    </li>\
+  ');
+
+  $('.c-subtabs__item .sort-by').click(function(e){
+    sortProducts($(this));
+    e.preventDefault();
+  });
 
 })();
