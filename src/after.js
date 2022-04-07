15f30ac5bb7ee626fc478b461ee3eb1db04b8044
Michal Mikolas <nanuqcz@gmail.com>
2022-04-07 16:28:39 +0200

    Added AJAX pagination support.

--- src/after.js
+++ src/after.js
@@ -1,15 +1,53 @@
 // this code will be executed after page load
 (function() {
+  //
+  // Init
+  //
   $ = jQuery;
 
-  $('.c-product-list__item.o-grid__item').each(function(){
-    $product = $(this);
-    $a = $product.find('a.c-product__image-link')
+  //
+  // Functions
+  //
+  refreshRating = function(){
+    $('.c-product-list__item.o-grid__item h3 .rating').remove();
 
-    $product.find('h3').append('<div class="rating"></div>');
-    $product.find('h3 .rating').load(
-      $a.attr('href') + ' .c-top-position__rating'
-    )
-  })
+    $('.c-product-list__item.o-grid__item').each(function(){
+      $product = $(this);
+      $a = $product.find('a.c-product__image-link')
+
+      $product.find('h3').append('<div class="rating"></div>');
+      $product.find('h3 .rating').load(
+        $a.attr('href') + ' .c-top-position__rating'
+      )
+    })
+  };
+
+  isListLoaded = function(){
+    return $('.is-fetching').length == 0;
+  }
+  isRatingLoaded = function(){
+    return $('.c-product-list__item.o-grid__item h3 .rating').length > 0;
+  }
+
+  refreshRatingManager = function(){
+    if (!isListLoaded()) {
+      setTimeout(refreshRatingManager, 1000);
+
+    } else if (isRatingLoaded()) {
+      return;
+
+    } else {
+      refreshRating();
+
+      $('.c-pagination a').click(function(){
+        setTimeout(refreshRatingManager, 1000);
+      });
+    }
+  };
+
+  //
+  // Do the job
+  //
+  refreshRatingManager();
 
 })();
