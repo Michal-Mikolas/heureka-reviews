2c45f2a2fb3f9383ae5a821b58f4ad96374a4b8b
Michal Mikolas <nanuqcz@gmail.com>
2022-04-08 17:46:39 +0200

    Added filter features.

--- src/after.js
+++ src/after.js
@@ -32,7 +32,7 @@
 
       $product.find('h3').append('<div class="rating"></div>');
       $product.find('h3 .rating').load(
-        $a.attr('href') + ' .c-top-position__rating', 
+        $a.attr('href') + ' .c-top-position__rating',
         function(){ refreshProductData($product); }
       )
 
@@ -68,6 +68,25 @@
     $(".c-product-list__items").html($products);
   };
 
+  var filterProducts = function($link){
+    var min_score = $('input[name=reviews_score_min]').val() * 1;
+    var min_amount = $('input[name=reviews_amount_min]').val() * 1;
+    var min_shops = $('input[name=reviews_shops_min]').val() * 1;
+
+    $('.c-product-list__item').each(function(){
+      $p = $(this);
+      if ($p.data('reviews-score') >= min_score
+        && $p.data('reviews-amount') >= min_amount
+        && $p.data('shops') >= min_shops
+      ) {
+        $p.show();
+
+      } else {
+        $p.hide();
+      }
+    });
+  };
+
 
   //
   // Do the job
@@ -75,7 +94,7 @@
 
   // Add stars summary
   refreshRatingManager();
-  
+
   // Add new sort features
   $('.c-subtabs__list').append('\
     <li class="c-subtabs__item">\
@@ -97,6 +116,46 @@
 
   $('.c-subtabs__item .sort-by').click(function(e){
     sortProducts($(this));
+    e.preventDefault();
+  });
+
+  // Add new filter features
+  $('.l-sidebar__filters.c-section-list').prepend('\
+    <li class="c-filter c-section-list__item">\
+      <form>\
+        <div class="c-form-cell">\
+          <label for="search-query-input" class="c-form-cell__label c-filter__title">\
+            Heureka Reviews\
+          </label>\
+          \
+          <div class="c-form-cell__input c-search-filter">\
+            <div class="c-search-filter__cell">\
+              <input class="e-input c-search-filter__input e-input--small" name="reviews_score_min" type="search" placeholder="Hodnocení min. %">\
+            </div>\
+            <button type="submit" class="e-button e-button--simple c-range-filter__button reviews_filter"><span>Ok</span></button>\
+          </div>\
+          \
+          <div class="c-form-cell__input c-search-filter">\
+            <div class="c-search-filter__cell">\
+              <input class="e-input c-search-filter__input e-input--small" name="reviews_amount_min" type="search" placeholder="Počet hodnocení min.">\
+            </div>\
+            <button type="submit" class="e-button e-button--simple c-range-filter__button reviews_filter"><span>Ok</span></button>\
+          </div>\
+          \
+          <div class="c-form-cell__input c-search-filter">\
+            <div class="c-search-filter__cell">\
+              <input class="e-input c-search-filter__input e-input--small" name="reviews_shops_min" type="search" placeholder="Počet obchodů min.">\
+            </div>\
+            <button type="submit" class="e-button e-button--simple c-range-filter__button reviews_filter"><span>Ok</span></button>\
+          </div>\
+          \
+        </div>\
+      </form>\
+    </li>\
+  ');
+
+  $('.l-sidebar__filters.c-section-list button.reviews_filter').click(function(e){
+    filterProducts($(this));
     e.preventDefault();
   });
 
